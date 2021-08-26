d1d673d9e59705e54c0ae629a0b2c65d2a5ba323
Perry <ch.parry1233@gmail.com>
2021-08-26 08:30:39 +0000

    small edit

--- BERT.py
+++ BERT.py
@@ -2,4 +2,4 @@ from transformers import BertTokenizer, BartForConditionalGeneration, Text2TextG
 tokenizer = BertTokenizer.from_pretrained("uer/bart-base-chinese-cluecorpussmall")
 model = BartForConditionalGeneration.from_pretrained("uer/bart-base-chinese-cluecorpussmall")
 text2text_generator = Text2TextGenerationPipeline(model, tokenizer)
-print(text2text_generator("你[MASK] [MASK]了", max_length=50, do_sample=False)[0]['generated_text'])
+print(text2text_generator("我[MASK]你[MASK]", max_length=50, do_sample=False)[0]['generated_text'])
